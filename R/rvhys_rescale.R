b6984b5b61f2af015a29fc19172d3c28e2409f1e
biovhys <48113222+biovhys@users.noreply.github.com>
2023-04-21 21:32:11 +0800

    1

--- R/rvhys_rescale.R
+++ R/rvhys_rescale.R
@@ -1,6 +1,6 @@
-#' @title rescale
+#' @title 0-1 rescale
 #' @description
-#' 自定义标准化函数
+#' 0-1 rescale
 #'
 #' @param x
 #' @param type
@@ -10,5 +10,11 @@
 #'
 #' @examples
 rvhys_rescale <- function(x, type = 1){
-
+  # type=1 正向指标, type=2 负向指标
+  rng = range(x, na.rm = TRUE)
+  if (type == 1) {
+    (x - rng[1]) / (rng[2] - rng[1])
+  } else {
+      (rng[2] - x) / (rng[2] - rng[1])
+  }
 }
