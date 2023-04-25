fcc4fb88438c7ffd55a6bc18d3a810c879b16020
biovhys <48113222+biovhys@users.noreply.github.com>
2023-04-25 11:14:16 +0800

    4.25

---
+++ R/rvhys_perm.R
@@ -0,0 +1,24 @@
+#' @title 置换检验
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' a <- runif(5, 1, 2)
+#' b <- runif(6, 4, 5)
+#' rvhys_perm(a, b)
+#' 
+rvhys_perm <- function(x, y, reps = 10000, seed = 20230425) {
+  set.seed(seed)
+  miu0 <- mean(y) - mean(x)
+  pooledData <- c(x, y)
+  meanDiff <- numeric(reps)
+  for (i in 1:reps) {
+    idx <- sample(1:length(pooledData), length(x), replace = FALSE)
+    x <- pooledData[idx]
+    y <- pooledData[-idx]
+    meanDiff[i] <- mean(y) - mean(x)
+  }
+  p <- mean(abs(meanDiff) >= abs(miu0))
+  return(p)
+}
--- R/rvhys_rescale.R
+++ R/rvhys_rescale.R
@@ -9,10 +9,10 @@
 #' @export
 #'
 #' @examples
-#' rvhys_rescale(c(1:5,7:22), min = 1, max = 2)
+#' rvhys_rescale(c(1:5,7:22, Inf), min = 1, max = 2)
 #'
 rvhys_rescale <- function(x, type = "pos", min = 0, max = 1){
-  rng = range(x, na.rm = TRUE)
+  rng = range(x, na.rm = TRUE, finite = TRUE)
   switch(type,
          "pos" = (max - min) * (x - rng[1]) / (rng[2] - rng[1]) + min,
          "neg" = (max - min) * (rng[2] - x) / (rng[2] - rng[1]) + min)
