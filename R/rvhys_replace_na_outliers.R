4e0c9150a7bae8af206c66cbc68a022a9c217ca1
biovhys <48113222+biovhys@users.noreply.github.com>
2023-05-05 11:36:55 +0800

    1

--- R/rvhys_replace_na_outliers.R
+++ R/rvhys_replace_na_outliers.R
@@ -10,7 +10,7 @@
 #' rvhys_replace_na_outliers(b)
 #' 
 rvhys_replace_na_outliers <- function(data, var) {
-  mutate(data, "{{var}}" := dplyr::if_else({{var}} > quantile({{var}}, probs = c(0.25, 0.75))[1] - 1.5 * IQR({{var}}) &
-                                             {{var}} < quantile({{var}}, probs = c(0.25, 0.75))[2] + 1.5 * IQR({{var}}), 
+  dplyr::mutate(data, "{{var}}" := dplyr::if_else({{var}} > stats::quantile({{var}}, probs = c(0.25, 0.75))[1] - 1.5 * stats::IQR({{var}}) &
+                                             {{var}} < stats::quantile({{var}}, probs = c(0.25, 0.75))[2] + 1.5 * stats::IQR({{var}}), 
                                            {{var}}, NA))
 }
